82194d1626292dfbc51c7a84f8aaf6f1ec6e4c64
Eric Ness <eness@blankmetal.ai>
2026-03-08 08:07:01 -0500

    Set up nix

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Next.js Foundations dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_24
+            pnpm
+            turbo
+          ];
+        };
+      });
+}
